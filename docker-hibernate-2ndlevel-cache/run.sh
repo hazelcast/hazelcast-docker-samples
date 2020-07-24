ac7d967034bd7b5b8d5b769d92e4bcf96f2dd6bd
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-07-24 14:42:49 +0200

    Remove outdated samples (#22)

--- docker-hibernate-2ndlevel-cache/run.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-mvn exec:java -Dexec.mainClass="com.hazelcast.hibernate.CreateTable" -Dexec.cleanupDaemonThreads=false
-mvn exec:java -Dexec.mainClass="com.hazelcast.hibernate.ManageEmployee"
